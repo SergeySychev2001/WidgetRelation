117d29bdc33426c5e51dcd1c3518cec207b85a8f
SergeySychev2001 <76484477+SergeySychev2001@users.noreply.github.com>
2025-06-05 22:09:34 +0300

    Restore dynamic child insertion

--- src/tasks/classes/HBoxContainer.ts
+++ src/tasks/classes/HBoxContainer.ts
@@ -3,47 +3,44 @@ import CssClasses from "../enums/CssClasses";
 import ContainerWidget from "./ContainerWidget";
 
 export default class HBoxContainer extends ContainerWidget {
+  public createDOM = (): HTMLDivElement => {
+    const el = document.createElement("div");
+    el.classList.add(CssClasses.CONTAINER);
+    el.classList.add(CssClasses.CONTAINER_H);
+    el.classList.add(this.getAlign());
 
-    public createDOM = (): HTMLDivElement => {
-        const el = document.createElement('div');
-        el.classList.add(CssClasses.CONTAINER);
-        el.classList.add(CssClasses.CONTAINER_H);
-        el.classList.add(this.getAlign());
-        this.getChildren().forEach(i => {
-            switch (i.getAlign()) {
-                case Align.alLeft:
-                    if (el.firstChild) {
-                        el.insertBefore(i.createDOM(), el.firstChild);
-                    } else {
-                        el.appendChild(i.createDOM());
-                    }
-                    break;
-                case Align.alClient:
-                    let firstRightChild: HTMLElement | null = null;
-
-                    for (let i = 0; i < el.childNodes.length; i++) {
-                        const child = el.childNodes.item(i);
-                        if (!(child instanceof HTMLElement)) throw new Error("Invalid Type");
-                        if (child.classList.contains(Align.alRight)) {
-                            firstRightChild = child;
-                            break;
-                        }
-                    }
-
-                    if (firstRightChild) {
-                        el.insertBefore(i.createDOM(), firstRightChild);
-                    } else {
-                        el.appendChild(i.createDOM());
-                    }
-
-                    break;
-                case Align.alRight:
-                    el.appendChild(i.createDOM());
-                    break;
-                default: throw new Error("Unexpected case");
+    for (const child of this.getChildren()) {
+      switch (child.getAlign()) {
+        case Align.alLeft:
+          if (el.firstChild) {
+            el.insertBefore(child.createDOM(), el.firstChild);
+          } else {
+            el.appendChild(child.createDOM());
+          }
+          break;
+        case Align.alClient: {
+          let firstRight: HTMLElement | null = null;
+          for (const node of Array.from(el.children)) {
+            if (node.classList.contains(Align.alRight)) {
+              firstRight = node as HTMLElement;
+              break;
             }
-        });
-        return el;
+          }
+          if (firstRight) {
+            el.insertBefore(child.createDOM(), firstRight);
+          } else {
+            el.appendChild(child.createDOM());
+          }
+          break;
+        }
+        case Align.alRight:
+          el.appendChild(child.createDOM());
+          break;
+        default:
+          throw new Error("Unexpected align");
+      }
     }
 
+    return el;
+  };
 }
